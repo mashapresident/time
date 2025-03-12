02bf92155e20d7fd4bfc9d0e91330b49b59810ff
Daniil <den291110@gmail.com>
2025-03-12 18:01:44 +0200

    gpio cleanup

--- test2.py
+++ test2.py
@@ -40,9 +40,9 @@ gpio.setcfg(EN, gpio.OUTPUT)
 try:
     while True:
         gpio.output(STEP, 1)
-        time.sleep(0.1)  # Час між імпульсами (чим менше, тим швидше)
+        time.time() * 1000 * 10
         gpio.output(STEP, 0)
-        time.sleep(0.1)
+        time.time() * 1000 * 10
 
 except KeyboardInterrupt:
     print ("Goodbye.")
