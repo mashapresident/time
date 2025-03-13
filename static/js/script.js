ce12c85adad1270953936f2c5bf23f20a16daf40
maria <konovalova132@icloud.com>
2025-03-13 10:58:16 +0200

    check set_steps value

--- static/js/script.js
+++ static/js/script.js
@@ -1,36 +1,12 @@
-function setSteps() {
-    var steps = document.getElementById("steps_per_revolution").value;
-    if(steps >= 0){
-        fetch('/set_steps', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            body: `steps_per_revolution=${steps}`
-        }).then(response => response.json())
-        .then(data => alert(`Кількість кроків на оберт: ${data.steps_per_revolution}`));
-    }
-    else{
-        alert("Помилка: кількість кроків не може бути від'ємною!");
-        return;
-    }
-}
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("set_steps").addEventListener("submit", function (event) {
+        let stepsInput = document.getElementById("steps_per_revolution");
+        let stepsValue = parseInt(stepsInput.value, 10);
 
-function calibrate(direction) {
-    var steps = document.getElementById("calibration_steps").value;
-    if(steps >= 0){
-        var delay = 500; // Затримка у мікросекундах
-        fetch('/calibrate', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
-            body: `direction=${direction}&steps=${steps}&delay=${delay}`
-        }).then(response => response.json())
-        .then(data => alert('Калібрування виконано'));
-    }
-    else{
-        alert("Помилка: кількість кроків не може бути від'ємною!");
-        return;
-    }
-}
+        if (isNaN(stepsValue) || stepsValue < 0) {
+            alert("Кількість кроків на оберт повинна бути невід’ємним числом!");
+            event.preventDefault();
+        }
+    });
+});
+
