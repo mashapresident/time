01ce59dd4dec62bd5b687dce372595a83a0d0c7a
maria <konovalova132@icloud.com>
2025-03-18 19:31:47 +0200

    wiringpy

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import wiringpi
 import time
 # Налаштовуємо GPIO-піни
-DIR = 6   # GPIO2 - Напрямок
-STEP = 11  # GPIO10 - Імпульси
-EN = 12    # GPIO2
+DIR = 2   # GPIO2 - Напрямок
+STEP = 1  # GPIO10 - Імпульси
+EN = 0    # GPIO2
 
 wiringpi.wiringPiSetup()
 wiringpi.pinMode(DIR, 1)
@@ -11,16 +11,11 @@ wiringpi.pinMode(STEP, 1)
 wiringpi.pinMode(EN, 1)
 
 def step(steps):
-    """
-    Асинхронна функція для керування кроками двигуна.
-    Блокуючі виклики time.sleep() замінено на await asyncio.sleep().
-    """
-
     if steps > 0:
         wiringpi.digitalWrite(DIR, 1)
         for _ in range(int(steps)):
             wiringpi.digitalWrite(STEP, 1)
-            time.sleep(0.01)  # асинхронна затримка
+            time.sleep(0.01)
             wiringpi.digitalWrite(STEP, 0)
             time.sleep(0.01)
     elif steps < 0:
