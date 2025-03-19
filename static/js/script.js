5776233060de9a382d4e89580dff4fb59cb4b79d
maria <konovalova132@icloud.com>
2025-03-19 17:56:53 +0200

    -

--- static/js/script.js
+++ static/js/script.js
@@ -9,6 +9,18 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("set_period").addEventListener("submit", function (event) {
+        let stepsInput = document.getElementById("period_per_revolution");
+        let stepsValue = parseInt(stepsInput.value, 10);
+
+        if (isNaN(stepsValue) || stepsValue <= 0) {
+            alert("Кількість кроків на оберт повинна бути додатнім числом!");
+            event.preventDefault();
+        }
+    });
+});
     fetch('/static/config.json')
     .then(function (response) {
         if (!response.ok) {
