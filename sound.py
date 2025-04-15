669f88090c5ee666c37dffa8ed6b1a2ec792ad8d
maria <konovalova132@icloud.com>
2025-04-15 13:04:21 +0300

    закоментувала код зі звуком

--- sound.py
+++ sound.py
@@ -1,6 +1,6 @@
 from time import sleep
 
-import vlc
+#import vlc
 import asyncio
 import time
 
@@ -10,24 +10,25 @@ def play_sound(filename):
     Синхронна функція для відтворення MP3-файлу за допомогою VLC.
     Функція блокує виконання, поки медіа не завершить відтворення.
     """
-    instance = vlc.Instance()
-    player = instance.media_player_new()
-    media = instance.media_new(filename)
-    player.set_media(media)
+    #instance = vlc.Instance()
+    #player = instance.media_player_new()
+    #media = instance.media_new(filename)
+    #player.set_media(media)
 
-    player.play()
+    #player.play()
     # Дамо плеєру кілька сотих секунди для старту відтворення
-    time.sleep(0.2)
-    while True:
-        state = player.get_state()
-        if state in (vlc.State.Ended, vlc.State.Stopped, vlc.State.Error):
-            break
-        time.sleep(0.1)
+    # time.sleep(0.2)
+    # while True:
+    #     #state = player.get_state()
+    #    # if state in (vlc.State.Ended, vlc.State.Stopped, vlc.State.Error):
+    #    #     break
+    #     time.sleep(0.1)
+    print("Грає звук із файлу " + filename)
 
 
 async def play(hour):
     try:
-        await asyncio.to_thread(play_sound, "music/files/melodiya.mp3")
+        await asyncio.to_thread(play_sound, clock.get_melody())
         for i in range(1, hour + 1):
             print(f"Відтворення звуку клаку — ітерація {i}")
             await asyncio.to_thread(play_sound, "music/files/stuk_audio.wav")
