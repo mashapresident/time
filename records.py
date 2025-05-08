5cba471f5e4ec50d3516b11b3e2b0ad6e0c27ea4
maria <konovalova132@icloud.com>
2025-05-08 14:35:43 +0300

    -

--- records.py
+++ records.py
@@ -1,2 +1,40 @@
+from quart import Quart, request, render_template, redirect, url_for, jsonify
+from load_config import *
+from main import app, UPLOAD_FOLDER
+import json
+import os
+
+@app.route('/save_event', methods=['POST'])
+async def save_event():
+    form = await request.form
+    name = form.get('name')
+    repeat = form.get('repeat')
+    time = form.get('time')
+    weekday = form.get('weekday', '')
+    date = form.get('date', '')
+
+    new_event = {
+        "date": date,
+        "dayOfWeek": weekday,
+        "filename": "",
+        "name": name,
+        "time": time
+    }
 
 
+
+    if not os.path.exists(RECORDS):
+        with open(RECORDS, 'w', encoding='utf-8') as f:
+            json.dump([], f, ensure_ascii=False, indent=4)
+
+    with open(RECORDS, 'r+', encoding='utf-8') as f:
+        try:
+            data = json.load(f)
+        except json.JSONDecodeError:
+            data = []
+        data.append(new_event)
+        f.seek(0)
+        json.dump(data, f, ensure_ascii=False, indent=4)
+        f.truncate()
+
+    return redirect(url_for('index'))
--- sound_events.py
+++ sound_events.py
@@ -1,29 +1,29 @@
-# from quart import Quart, request, render_template, redirect, url_for, jsonify
-# from load_config import *
-# from main import app
-#
-#
-# @app.route('/upload_melody', methods=['POST'])
-# async def upload_melody():
-#     files = await request.files
-#     melody_file = files.get('melody')
-#     if melody_file:
-#         fixed_filename = "melody.mp3"
-#         file_path = os.path.join(UPLOAD_FOLDER, fixed_filename)
-#         await melody_file.save(file_path)
-#         return redirect(url_for('index'))
-#     else:
-#         return {"error": "No melody sound file provided"}, 400
-#
-#
-# @app.route('/upload_knock', methods=['POST'])
-# async def upload_knock():
-#     files = await request.files
-#     knock_file = files.get('knock')
-#     if knock_file:
-#         fixed_filename = "knock.mp3"
-#         file_path = os.path.join(UPLOAD_FOLDER, fixed_filename)
-#         await knock_file.save(file_path)
-#         return redirect(url_for('index'))
-#     else:
-#         return {"error": "No knock sound file provided"}, 400
+from quart import Quart, request, render_template, redirect, url_for, jsonify
+from load_config import *
+from main import app
+
+
+@app.route('/upload_melody', methods=['POST'])
+async def upload_melody():
+    files = await request.files
+    melody_file = files.get('melody')
+    if melody_file:
+        fixed_filename = "melody.mp3"
+        file_path = os.path.join(UPLOAD_FOLDER, fixed_filename)
+        await melody_file.save(file_path)
+        return redirect(url_for('index'))
+    else:
+        return {"error": "No melody sound file provided"}, 400
+
+
+@app.route('/upload_knock', methods=['POST'])
+async def upload_knock():
+    files = await request.files
+    knock_file = files.get('knock')
+    if knock_file:
+        fixed_filename = "knock.mp3"
+        file_path = os.path.join(UPLOAD_FOLDER, fixed_filename)
+        await knock_file.save(file_path)
+        return redirect(url_for('index'))
+    else:
+        return {"error": "No knock sound file provided"}, 400
