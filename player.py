fca1bd66b58808fec357907a8c384b58c4389905
maria <konovalova132@icloud.com>
2025-05-23 16:11:17 +0300

    FINALLL

--- player.py
+++ player.py
@@ -2,13 +2,13 @@ import vlc
 from time import *
 import asyncio
 import time
-from load_config import UPLOAD_FOLDER
-def play_melody(filename: str, knock: bool, hour: int):
+from load_config import UPLOAD_FOLDER, REGULAR_MUSIC_FOLDER
+async def play_melody(filename: str, knock: bool, hour: int):
     """
     Відтворює MP3-файл за допомогою VLC, блокуючи виконання, поки файл не завершиться.
     Якщо `knock=True`, після мелодії запускається відтворення звуку клаку `hour` разів.
     """
-    filepath = UPLOAD_FOLDER + filename
+    filepath = UPLOAD_FOLDER + "/" + filename
     instance = vlc.Instance()
     player = instance.media_player_new()
     media = instance.media_new(filepath)
@@ -29,4 +29,16 @@ async def play_knock(hour: int):
     Асинхронно відтворює звук клаку `hour` разів із паузою між кожним ударом.
     """
     for i in range(hour):
-        time.sleep(0.5)
+        filepath = REGULAR_MUSIC_FOLDER + "/knock.mp3"
+        instance = vlc.Instance()
+        player = instance.media_player_new()
+        media = instance.media_new(filepath)
+        player.set_media(media)
+
+        player.play()
+        time.sleep(0.2)
+
+        while player.get_state() not in (vlc.State.Ended, vlc.State.Stopped, vlc.State.Error):
+            time.sleep(0.1)
+
+        await asyncio.sleep(0.5)
