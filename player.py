0a2afaa3a4ce4b0129a6f3ff562a0d9e5a9879d3
maria <konovalova132@icloud.com>
2025-05-30 13:03:40 +0300

    -

--- player.py
+++ player.py
@@ -1,37 +1,28 @@
-import vlc
 import asyncio
 import os
 from load_config import UPLOAD_FOLDER, REGULAR_MUSIC_FOLDER
 
+async def play_with_mpv(filepath):
+    if not os.path.exists(filepath):
+        print(f"[ERROR] Файл не знайдено: {filepath}")
+        return
+
+    process = await asyncio.create_subprocess_exec(
+        "mpv", "--no-terminal", "--quiet", filepath,
+        stdout=asyncio.subprocess.DEVNULL,
+        stderr=asyncio.subprocess.DEVNULL
+    )
+    await process.communicate()
+
 async def play_melody(filename: str, knock: bool, hour: int):
     filepath = os.path.join(UPLOAD_FOLDER, filename)
-    instance = vlc.Instance('--aout=alsa') 
-    player = instance.media_player_new()
-    media = instance.media_new(filepath)
-    player.set_media(media)
-
-    player.play()
-    await asyncio.sleep(0.2)
-
-    while player.get_state() not in (vlc.State.Ended, vlc.State.Stopped, vlc.State.Error):
-        await asyncio.sleep(0.1)
+    await play_with_mpv(filepath)
 
     if knock:
         await play_knock(hour)
 
-
 async def play_knock(hour: int):
+    filepath = os.path.join(REGULAR_MUSIC_FOLDER, "knock.mp3")
     for i in range(hour):
-        filepath = os.path.join(REGULAR_MUSIC_FOLDER, "knock.mp3")
-        instance = vlc.Instance()
-        player = instance.media_player_new()
-        media = instance.media_new(filepath)
-        player.set_media(media)
-
-        player.play()
-        await asyncio.sleep(0.2)
-
-        while player.get_state() not in (vlc.State.Ended, vlc.State.Stopped, vlc.State.Error):
-            await asyncio.sleep(0.1)
-
+        await play_with_mpv(filepath)
         await asyncio.sleep(0.5)
